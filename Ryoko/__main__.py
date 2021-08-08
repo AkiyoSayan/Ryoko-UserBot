e6835ba55ac05ee3020b5349f9f39a0b78ca14ec
AkiyoSayan <88580652+AkiyoSayan@users.noreply.github.com>
2021-08-08 21:33:21 +0530

    Update __main__.py

--- Ryoko/__main__.py
+++ Ryoko/__main__.py
@@ -1,4 +1,6 @@
 from pyrogram import idle, Client, filters
+from platform import python_version
+from pyrogram import __version__
 from Ryoko import app, LOGGER
 from Ryoko import Ryoko_version
 import logging
@@ -7,4 +9,8 @@ app.start()
 me = app.get_me()
 full_info = f"""Ryoko A Telegram Userbot Is Started.
 -> Ryoko Version: {Ryoko_version} 
+-> Python: {python_version()}
+-> Pyrogram Version: {__version__}
+"""
+print(full_info)
 idle()
